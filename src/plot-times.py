2cb6c0fb9f2ce22bf3c199db954be55059d43541
Jerome White <jerome@thewex.net>
2025-01-16 14:41:34 +0530

    Take into account new data format
    Results now distinguish the two phases of prompting: full answer then
    summarization. Plots now visualize all times.

--- src/plot-times.py
+++ src/plot-times.py
@@ -1,11 +1,22 @@
 import sys
 from pathlib import Path
 from argparse import ArgumentParser
+from dataclasses import dataclass
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+@dataclass
+class PlotLine:
+    name: str
+    color: tuple
+
+def lines(ax):
+    legend = ax.get_legend()
+    for (t, l) in zip(legend.get_texts(), legend.get_lines()):
+        yield PlotLine(t.get_text(), l.get_color())
 
 if __name__ == "__main__":
     arguments = ArgumentParser()
@@ -13,26 +24,41 @@ if __name__ == "__main__":
     arguments.add_argument('--output', type=Path)
     args = arguments.parse_args()
 
-    x = 'duration'
+    hue = 'response'
     df = pd.read_csv(sys.stdin)
-    ax = sns.ecdfplot(x=x, data=df)
+
+    fig = plt.gcf()
+    (width, height) = fig.get_size_inches()
+    fig.set_size_inches(width * 1.5, height)
+
+    sns.set_palette('colorblind')
+    ax = sns.ecdfplot(
+        x='seconds',
+        hue=hue,
+        data=df,
+    )
 
     ax.set_xlabel('Duration (sec)')
 
     ax.grid(axis='both', which='major', alpha=0.3)
-    ax.grid(axis='y', which='minor', alpha=0.25, linestyle='dotted')
+    ax.grid(axis='both', which='minor', alpha=0.25, linestyle='dotted')
 
     ax.xaxis.set_major_locator(MultipleLocator(base=2))
+    ax.xaxis.set_minor_locator(MultipleLocator())
     ax.yaxis.set_major_locator(MultipleLocator(base=0.1))
     ax.yaxis.set_minor_locator(AutoMinorLocator())
 
-    ax.axvline(
-        x=df[x].mean(),
-        linestyle='dashed',
-        color='red',
-        alpha=0.3,
-    )
     if args.cutoff is not None:
         ax.set_xlim(0, args.cutoff)
+
+    view = df.groupby(hue).mean()
+    for l in lines(ax):
+        x = view.loc[l.name].item()
+        ax.axvline(
+            x=x,
+            linestyle='dashed',
+            color=l.color,
+            alpha=0.45,
+        )
 
     plt.savefig(args.output, bbox_inches='tight')
